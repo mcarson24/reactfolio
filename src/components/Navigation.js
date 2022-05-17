b3a0a86f9d9707b10b3daddbbb409486d8ae8ead
Matthew Carson <matthew.carson@pm.me>
2022-05-17 02:54:51 -0400

    Start Navigation component

---
+++ src/components/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navigation.css'
+
+const Navigation = () => (
+  <nav>
+    <ul>
+      <li>About Me</li>
+      <li>Portfolio</li>
+      <li>Contact</li>
+      <li>Resume</li>
+    </ul>
+  </nav>
+)
+
+export default Navigation
