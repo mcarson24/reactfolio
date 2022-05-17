16708eac4527b7e15fe0a1eec3a6e240b0948bbc
Matthew Carson <matthew.carson@pm.me>
2022-05-17 03:14:12 -0400

    Basic navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,38 @@
-import React from 'react'
-import './Navigation.css'
+import React, {useState} from 'react'
 
-const Navigation = () => (
-  <nav>
-    <ul>
-      <li>About Me</li>
-      <li>Portfolio</li>
-      <li>Contact</li>
-      <li>Resume</li>
-    </ul>
-  </nav>
-)
+const Navigation = () => {
+  const [currentTab, setCurrentTab] = useState('About Me')
+
+  return (
+    <nav className="mt-6 h-12">
+      <ul className="flex justify-end h-full">
+        <li className={currentTab === 'About Me' ? 'mr-8 text-purple-600 font-bold border-b-4 border-purple-600' : 'mr-8'}>
+          <a href="#about"
+             onClick={() => setCurrentTab('About Me')}>
+            About Me
+          </a>
+        </li>
+        <li className={currentTab === 'Portfolio' ? 'mr-8 text-purple-600 font-bold border-b-4 border-purple-600' : 'mr-8'}>
+          <a href="#portfolio"
+             onClick={() => setCurrentTab('Portfolio')}>
+            Portfolio
+          </a>
+        </li>
+        <li className={currentTab === 'Contact' ? 'mr-8 text-purple-600 font-bold border-b-4 border-purple-600' : 'mr-8'}>
+          <a href="#contact"
+             onClick={() => setCurrentTab('Contact')}>
+            Contact
+          </a>
+        </li>
+        <li className={currentTab === 'Resume' ? 'mr-8 text-purple-600 font-bold border-b-4 border-purple-600' : 'mr-8'}>
+          <a href="#resume"
+             onClick={() => setCurrentTab('Resume')}>
+            Resume
+          </a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
 
 export default Navigation
